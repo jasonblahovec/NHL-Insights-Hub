b49ca82e60006717206d532bbfdc2dd96b73c0fe
Jason Blahovec <jason.blahovec@gmail.com>
2024-01-17 07:43:19 -0500

    ____

--- nhl_api_november_2023/analysis/corsi/dataproc_corsi_from_nhl_plays.py
+++ nhl_api_november_2023/analysis/corsi/dataproc_corsi_from_nhl_plays.py
@@ -239,7 +239,7 @@ class PlayerGameCorsi():
         ])).withColumn("team", f.lit(self.player_team))
 
     def run_all_team_analysis(self):
-        for _team_i, team in enumerate(self.teams_key.collect()):
+        for _team_i, team in enumerate(self.teams_key.collect()[0:10]):
             self.set_team(team.side)
 
             # must update df players key inside this function.:
