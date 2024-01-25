7b62ef1263fceacb7be8475f85a713109ec03a7e
Jason Blahovec <jason.blahovec@gmail.com>
2024-01-24 20:06:46 -0500

    _

--- nhl_api_november_2023/analysis/corsi/prepare_corsi_input_data.py
+++ nhl_api_november_2023/analysis/corsi/prepare_corsi_input_data.py
@@ -38,11 +38,15 @@ if __name__ == "__main__":
     df_forwards_input = spark.read.format("parquet").load(f"gs://{input_bucket_name}/{input_fs_forwards}").withColumn("hashmod", f.expr(f"abs(hash(cast(game_id as float))) % {hm_count}")).distinct()
     df_defense_input = spark.read.format("parquet").load(f"gs://{input_bucket_name}/{input_fs_defense}").withColumn("hashmod", f.expr(f"abs(hash(cast(game_id as float))) % {hm_count}")).distinct()
 
-    # some duplication exists in the 2021_2022 season (game_id 0500 - 0900).  Handling here:
-    df_plays_input.repartition('hashmod').write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_plays}", mode = 'overwrite')
+    df_plays_input.write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_plays}", mode = 'overwrite')
 
-    df_forwards_input.distinct().repartition('hashmod').write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_forwards}", mode = 'overwrite')
+    df_forwards_input.write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_forwards}", mode = 'overwrite')
 
-    df_defense_input.distinct().repartition('hashmod').write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_defense}", mode = 'overwrite')
+    df_defense_input.write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_defense}", mode = 'overwrite')
+    # df_plays_input.repartition('hashmod').write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_plays}", mode = 'overwrite')
+
+    # df_forwards_input.repartition('hashmod').write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_forwards}", mode = 'overwrite')
+
+    # df_defense_input.repartition('hashmod').write.format("parquet").save(f"gs://{output_bucket_name}/{output_fs_defense}", mode = 'overwrite')
 
     spark.stop()
