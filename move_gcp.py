6924eb7590d8d38916cd4a26f6a222bbfc4d3394
Jason Blahovec <jason.blahovec@gmail.com>
2024-01-26 09:10:04 -0500

    __

--- move_gcp.py
+++ move_gcp.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
         # [f"gs://nhl-api-output/output_plays/plays_sparse_detail", f"gs://nhl-seasons/2022_2023/plays_sparse_detail"]
         # ,[f"gs://nhl-api-output/output_plays/plays_with_onice", f"gs://nhl-seasons/2022_2023/plays_with_onice"]
         # ,[f"gs://nhl-api-output/output_plays/raw_plays", f"gs://nhl-seasons/2022_2023/raw_plays"]
-        [f"gs://nhl-api-output/output_plays/corsi/input_parquet/partitioned_forwards_2022_season", f"gs://nhl-seasons/2022_2023/corsi/input_parquet/partitioned_forwards"]
-        ,[f"gs://nhl-api-output/output_plays/corsi/input_parquet/patritioned_defense_2022_season", f"gs://nhl-seasons/2022_2023/corsi/input_parquet/partitioned_defense"]
-        ,[f"gs://nhl-api-output/output_plays/corsi/input_parquet/partitioned_goalies_2022_season", f"gs://nhl-seasons/2022_2023/corsi/input_parquet/partitioned_goalies"]
-        ,[f"gs://nhl-api-output/output_plays/corsi/2022_full_season/team_player_game_detail", f"gs://nhl-seasons/2022_2023/corsi/full_season/team_player_game_detail"]
-        ,[f"gs://nhl-api-output/output_plays/corsi/2022_full_season/team_player_summary", f"gs://nhl-seasons/2022_2023/corsi/full_season/team_player_summary"]
+        [f"gs://nhl-api-output/corsi/input_parquet/partitioned_forwards_2022_season", f"gs://nhl-seasons/2022_2023/corsi/input_parquet/partitioned_forwards"]
+        ,[f"gs://nhl-api-output/corsi/input_parquet/patritioned_defense_2022_season", f"gs://nhl-seasons/2022_2023/corsi/input_parquet/partitioned_defense"]
+        ,[f"gs://nhl-api-output/corsi/input_parquet/partitioned_goalies_2022_season", f"gs://nhl-seasons/2022_2023/corsi/input_parquet/partitioned_goalies"]
+        ,[f"gs://nhl-api-output/corsi/2022_full_season/team_player_game_detail", f"gs://nhl-seasons/2022_2023/corsi/full_season/team_player_game_detail"]
+        ,[f"gs://nhl-api-output/corsi/2022_full_season/team_player_summary", f"gs://nhl-seasons/2022_2023/corsi/full_season/team_player_summary"]
     ]
 
     for tbs in tables:
